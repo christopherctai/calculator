8a89822bb2898d6d84583c35480bd527805c2314
Chris Tai <christopherctai@gmail.com>
2023-01-13 17:11:41 -0800

    Create operate function

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
-function add (a, b) {
+function add(a, b) {
     return a + b;
 }
 
-function subtract (a, b) {
+function subtract(a, b) {
     return a - b;
 }
 
-function multiply (a, b) {
+function multiply(a, b) {
     return Math.round((a * b) * 1000) / 1000;
 }
 
-function divide (a, b) {
+function divide(a, b) {
     return Math.round(1000 * (a / b)) / 1000;
 }
 
+function operate(operator, firstNumber, secondNumber) {
+    if (operator === "+") {
+        return add(firstNumber, secondNumber);
+    } else if (operator === "-") {
+        return subtract(firstNumber, secondNumber);
+    } else if (operator === "*") {
+        return multiply(firstNumber, secondNumber);
+    } else if (operator === "/") {
+        return divide(firstNumber, secondNumber);
+    } else {
+        return "ERROR";
+    }
+}
