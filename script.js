364df1baf7e738b6cbc8591f382382fa3cf1d688
Chris Tai <christopherctai@gmail.com>
2023-01-13 17:06:45 -0800

    Define basic calculation functions

---
+++ script.js
@@ -0,0 +1,16 @@
+function add (a, b) {
+    return a + b;
+}
+
+function subtract (a, b) {
+    return a - b;
+}
+
+function multiply (a, b) {
+    return Math.round((a * b) * 1000) / 1000;
+}
+
+function divide (a, b) {
+    return Math.round(1000 * (a / b)) / 1000;
+}
+
